20f56615abc344d4d02e29f66be9eb254b0c28ee
Alyyyy88 <ali_20235078@fci.helwan.edu.eg>
2025-02-17 18:12:12 +0200

    Com

---
+++ script.js
@@ -0,0 +1,127 @@
+document.addEventListener("DOMContentLoaded", function () {
+const form = document.querySelector(".multi-form");
+
+const btn = document.querySelector(".btn");
+
+const step = document.querySelectorAll(".step");
+
+const username = document.querySelector("#username");
+
+const email = document.querySelector("#em");
+
+const errUser = document.querySelector(".err-user");
+
+const errEmail =document.querySelector(".err-email");
+
+const invalidEmailMsg = document.querySelector(".inv-email");
+
+const divBtns = document.querySelectorAll(".option");
+
+const list = document.querySelector("ul");
+
+
+const spUser = document.createElement('span');
+const spEmail = document.createElement('span');
+const pUser = document.querySelector('.p-user-info');
+const pEmail = document.querySelector('.p-email-info');
+
+const successMsg = document.querySelector(".success-msg");
+
+
+
+
+let stepIndx = 0;
+
+function isValidEmail(emailValue) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailPattern.test(emailValue);
+}
+
+
+
+btn.addEventListener("click",function(e){
+  e.preventDefault();
+
+  divBtns.forEach(divBtn => { 
+    let isChecked = false;
+  divBtn.addEventListener("click", function() {
+    if(!isChecked){
+      divBtn.id = "selected";
+      isChecked = true;
+      const li = document.createElement('li');
+      li.textContent = divBtn.textContent;
+      list.append(li);
+    }
+   
+    
+  });
+});
+
+  if(stepIndx == 0 ){
+    if(username.value.trim() === ""){
+      username.style.border = "2px solid red";
+      errUser.style.display = "block";
+      return;
+    }else{
+      username.style.border = ""; 
+      errUser.style.display = "none";
+      spUser.textContent = username.value;
+    }
+    if(email.value.trim() === ""){
+      email.style.border = "2px solid red";
+      errEmail.style.display = "block";
+      return;
+    }else if(!isValidEmail(email.value)){
+      email.style.border = "2px solid red";
+      invalidEmailMsg.style.display = "block";
+      return;
+
+    }else{
+      email.style.border = "";
+      errEmail.style.display = "none";
+      invalidEmailMsg.style.display = "none";
+      spEmail.textContent = email.value;
+
+    }
+  }
+
+  if(stepIndx < step.length - 1){
+    step[stepIndx].style.display = "none";
+    stepIndx++;
+    step[stepIndx].style.display = "block";
+    
+  }
+
+  if (stepIndx === step.length - 1  ) {
+    btn.textContent = "Submit";
+    pUser.append(spUser);
+    pEmail.append(spEmail);
+  }
+
+  if(btn.textContent == "Submit"){
+    form.reset();
+    alert("Success")
+  }
+  
+});
+  
+
+username.addEventListener("input",function(){
+    if(username.value.trim() === ""){
+      username.style.border = "2px solid red";
+    }
+    else{
+      username.style.border = "";
+    }
+});
+
+email.addEventListener("input",function(){
+  if(email.value.trim() === ""){
+    email.style.border = "2px solid red";
+  }
+  else{
+    email.style.border = "";
+  }
+});
+});
+
